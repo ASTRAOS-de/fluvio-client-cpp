068ce80b8ddc5baa9cfe6daa0fde3d5bc776c3cd
KunoVonHagen <41991675+KunoVonHagen@users.noreply.github.com>
2026-05-31 16:20:57 +0200

    Updated API to object-oriented design

--- src/config.rs
+++ src/config.rs
@@ -1,73 +1,90 @@
-use fluvio::FluvioConfig;
-use fluvio::consumer::ConsumerConfigBuilder;
-use fluvio::TopicProducerConfigBuilder;
+use std::ops::DerefMut;
+use fluvio::FluvioConfig as FluvioConfigNative;
+use fluvio::consumer::ConsumerConfigBuilder as ConsumerConfigBuilderNative;
+use fluvio::TopicProducerConfigBuilder as TopicProducerConfigBuilderNative;
 
-pub struct FluvioConfigWrapper { pub inner: FluvioConfig }
-pub struct ConsumerConfigWrapper { pub inner: ConsumerConfigBuilder }
-pub struct ProducerConfigWrapper { pub inner: TopicProducerConfigBuilder }
+pub struct ConsumerConfigBuilder { pub inner: ConsumerConfigBuilderNative }
+pub struct TopicProducerConfigBuilder { pub inner: TopicProducerConfigBuilderNative }
+pub struct FluvioConfig { pub inner: FluvioConfigNative }
 
-pub fn producer_config_new() -> Box<ProducerConfigWrapper> {
-    Box::new(ProducerConfigWrapper { inner: TopicProducerConfigBuilder::default() })
+impl TopicProducerConfigBuilder {
+    pub fn create() -> Box<TopicProducerConfigBuilder> {
+        Box::new(TopicProducerConfigBuilder { inner: TopicProducerConfigBuilderNative::default() })
+    }
+    pub fn batch_size(self: &mut Self, size: usize) -> Box<TopicProducerConfigBuilder> {
+        self.inner.batch_size(size);
+        Box::new(TopicProducerConfigBuilder { inner: self.inner.clone() })
+    }
+    pub fn linger(self: &mut Self, linger: u64) -> Box<TopicProducerConfigBuilder> {
+        self.inner.linger(std::time::Duration::from_millis(linger));
+        Box::new(TopicProducerConfigBuilder { inner: self.inner.clone() })
+    }
 }
 
-pub fn consumer_config_new() -> Box<ConsumerConfigWrapper> {
-    Box::new(ConsumerConfigWrapper { inner: ConsumerConfigBuilder::default() })
+impl ConsumerConfigBuilder {
+    pub fn create() -> Box<ConsumerConfigBuilder> {
+        Box::new(ConsumerConfigBuilder { inner: ConsumerConfigBuilderNative::default() })
+    }
+    pub fn max_bytes(self: &mut Self, max: i32) -> Box<ConsumerConfigBuilder> {
+        self.inner.max_bytes(max);
+        Box::new(ConsumerConfigBuilder { inner: self.inner.clone() })
+    }
+    pub fn disable_continuous(self: &mut Self, val: bool) -> Box<ConsumerConfigBuilder> {
+        self.inner.disable_continuous(val);
+        Box::new(ConsumerConfigBuilder { inner: self.inner.clone() })
+    }
+
 }
 
-pub fn consumer_config_max_bytes(c: &mut ConsumerConfigWrapper, max: i32) {
-    c.inner.max_bytes(max);
+impl FluvioConfig {
+    pub fn create(addr: &str) -> Box<FluvioConfig> {
+        Box::new(FluvioConfig { inner: FluvioConfigNative::new(addr) })
+    }
+    pub fn load() -> Result<Box<FluvioConfig>, String> {
+        FluvioConfigNative::load().map(|c| Box::new(FluvioConfig { inner: c })).map_err(|e| e.to_string())
+    }
+    pub fn set_endpoint(self: &mut Self, endpoint: &str) {
+        self.inner.endpoint = endpoint.to_string();
+    }
+    pub fn set_client_id(self: &mut Self, client_id: &str) {
+        self.inner.client_id = Some(client_id.to_string());
+    }
+    pub fn disable_tls(self: &mut Self) {
+        self.inner.tls = fluvio::config::TlsPolicy::Disabled;
+    }
+    pub fn set_anonymous_tls(self: &mut Self) {
+        self.inner.tls = fluvio::config::TlsPolicy::Anonymous;
+    }
+    pub fn set_inline_tls(self: &mut Self, domain: &str, key: &str, cert: &str, ca_cert: &str) {
+        self.inner.tls = fluvio::config::TlsPolicy::Verified(fluvio::config::TlsConfig::Inline(fluvio::config::TlsCerts {
+            domain: domain.to_string(),
+            key: key.to_string(),
+            cert: cert.to_string(),
+            ca_cert: ca_cert.to_string(),
+        }));
+    }
+    pub fn set_tls_file_paths(self: &mut Self, domain: &str, key_path: &str, cert_path: &str, ca_cert_path: &str) {
+        self.inner.tls = fluvio::config::TlsPolicy::Verified(fluvio::config::TlsConfig::Files(fluvio::config::TlsPaths {
+            domain: domain.to_string(),
+            key: std::path::PathBuf::from(key_path),
+            cert: std::path::PathBuf::from(cert_path),
+            ca_cert: std::path::PathBuf::from(ca_cert_path),
+        }));
+    }
 }
 
-pub fn consumer_config_disable_continuous(c: &mut ConsumerConfigWrapper, val: bool) {
-    c.inner.disable_continuous(val);
-}
 
-pub fn producer_config_batch_size(c: &mut ProducerConfigWrapper, size: usize) {
-    c.inner.batch_size(size);
-}
 
-pub fn producer_config_linger(c: &mut ProducerConfigWrapper, linger: u64) {
-    c.inner.linger(std::time::Duration::from_millis(linger));
-}
 
-pub fn fluvio_config_new(addr: &str) -> Box<FluvioConfigWrapper> {
-    Box::new(FluvioConfigWrapper { inner: FluvioConfig::new(addr) })
-}
 
-pub fn fluvio_config_load() -> Result<Box<FluvioConfigWrapper>, String> {
-    FluvioConfig::load().map(|c| Box::new(FluvioConfigWrapper { inner: c })).map_err(|e| e.to_string())
-}
 
-pub fn fluvio_config_set_endpoint(c: &mut FluvioConfigWrapper, endpoint: &str) {
-    c.inner.endpoint = endpoint.to_string();
-}
 
-pub fn fluvio_config_set_client_id(c: &mut FluvioConfigWrapper, client_id: &str) {
-    c.inner.client_id = Some(client_id.to_string());
-}
 
-pub fn fluvio_config_disable_tls(c: &mut FluvioConfigWrapper) {
-    c.inner.tls = fluvio::config::TlsPolicy::Disabled;
-}
 
-pub fn fluvio_config_set_anonymous_tls(c: &mut FluvioConfigWrapper) {
-    c.inner.tls = fluvio::config::TlsPolicy::Anonymous;
-}
 
-pub fn fluvio_config_set_inline_tls(c: &mut FluvioConfigWrapper, domain: &str, key: &str, cert: &str, ca_cert: &str) {
-    c.inner.tls = fluvio::config::TlsPolicy::Verified(fluvio::config::TlsConfig::Inline(fluvio::config::TlsCerts {
-        domain: domain.to_string(),
-        key: key.to_string(),
-        cert: cert.to_string(),
-        ca_cert: ca_cert.to_string(),
-    }));
-}
 
-pub fn fluvio_config_set_tls_file_paths(c: &mut FluvioConfigWrapper, domain: &str, key_path: &str, cert_path: &str, ca_cert_path: &str) {
-    c.inner.tls = fluvio::config::TlsPolicy::Verified(fluvio::config::TlsConfig::Files(fluvio::config::TlsPaths {
-        domain: domain.to_string(),
-        key: std::path::PathBuf::from(key_path),
-        cert: std::path::PathBuf::from(cert_path),
-        ca_cert: std::path::PathBuf::from(ca_cert_path),
-    }));
-}
+
+
+
+
+
--- src/producer.rs
+++ src/producer.rs
@@ -1,32 +1,22 @@
-use fluvio::TopicProducerPool;
+use fluvio::TopicProducerPool as TopicProducerPoolNative;
 use fluvio_future::task::run_block_on;
-use crate::config::ProducerConfigWrapper;
-use crate::client::FluvioClient;
-use crate::produce_output::FluvioProduceOutput;
+use crate::produce_output::ProduceOutput;
 
-pub struct FluvioProducer { pub inner: TopicProducerPool }
+pub struct TopicProducerPool { pub inner: TopicProducerPoolNative }
 
-pub fn create_producer(client: &FluvioClient, topic: &str) -> Result<Box<FluvioProducer>, String> {
-    run_block_on(client.inner.topic_producer(topic))
-        .map(|producer| Box::new(FluvioProducer { inner: producer }))
-        .map_err(|e| e.to_string())
+impl TopicProducerPool {
+    pub fn send(self: &Self, key: &[u8], value: &[u8]) -> Result<Box<ProduceOutput>, String> {
+        run_block_on(self.inner.send(key, value))
+            .map(|out| Box::new(ProduceOutput { inner: Some(out) }))
+            .map_err(|e| e.to_string())
+    }
+    pub fn flush(self: &Self) -> Result<(), String> {
+        run_block_on(self.inner.flush())
+            .map(|_| ())
+            .map_err(|e| e.to_string())
+    }
+
 }
 
-pub fn create_producer_with_config(client: &FluvioClient, topic: &str, config: &ProducerConfigWrapper) -> Result<Box<FluvioProducer>, String> {
-    let built_config = config.inner.build().map_err(|e| e.to_string())?;
-    run_block_on(client.inner.topic_producer_with_config(topic, built_config))
-        .map(|producer| Box::new(FluvioProducer { inner: producer }))
-        .map_err(|e| e.to_string())
-}
 
-pub fn producer_send(producer: &FluvioProducer, key: &[u8], value: &[u8]) -> Result<Box<FluvioProduceOutput>, String> {
-    run_block_on(producer.inner.send(key, value))
-        .map(|out| Box::new(FluvioProduceOutput { inner: Some(out) }))
-        .map_err(|e| e.to_string())
-}
 
-pub fn producer_flush(producer: &FluvioProducer) -> Result<(), String> {
-    run_block_on(producer.inner.flush())
-        .map(|_| ())
-        .map_err(|e| e.to_string())
-}
